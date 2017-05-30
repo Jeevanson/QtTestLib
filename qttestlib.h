a581d1df96f668ac34ba1fdd9ef782b59c30bb02
Jeevanson <jmahathuvam@encoress.com>
2017-05-30 18:06:01 +0530

    added qString example

--- qttestlib.h
+++ qttestlib.h
@@ -1,5 +1,6 @@
 #ifndef QTTESTLIB_H
 #define QTTESTLIB_H
+#include <QtCore>
 
 
 class QtTestLib
@@ -8,6 +9,7 @@ class QtTestLib
 public:
     QtTestLib();
     float getArea(float width,float height);
+    QString getHelloString();
 };
 
 #endif // QTTESTLIB_H
